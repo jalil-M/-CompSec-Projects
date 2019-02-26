f320844ef95780ddf047940fa2542066af8cc4a1
Martin Larsson <dat14mla@student.lu.se>
2019-02-26 12:27:43 +0100

    server work and datahandler skeleton

---
+++ project2/src/DataHandler.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Handles record management Read, write, create, store, delete
+ * 
+ */
+public class DataHandler {
+
+	public DataHandler(String directoryPath, int UID) {
+	}
+
+	public DataHandler(File userfile) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void putRequest(String[] clientCmd) {
+		// TODO Auto-generated method stub
+		switch (clientCmd[0]) {
+		case "read":
+			break;
+		case "write":
+			break;
+		case "delete":
+			break;
+		case "ls":
+			break;
+		case "create":
+			break;
+		}
+
+	}
+
+	public void handleRequest(String clientMsg, PrintWriter out) {
+		// TODO Auto-generated method stub
+		out.println("to be printed");
+		out.flush();
+	}
+
+}
